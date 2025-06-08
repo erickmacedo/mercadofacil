a4abe5e26d42a7932951f3aae4ae1c3bd843fe00
erickmacedo <erick98macedo@gmail.com>
2025-06-08 00:35:07 -0300

    Criação da Rota de Get para Produtos

--- backendmf/src/produtos/produtos.controller.ts
+++ backendmf/src/produtos/produtos.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { 
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProdutosService } from './produtos.service';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
@@ -12,7 +20,7 @@ export class ProdutosController {
     return this.produtosService.create(createProdutoDto);
   }
 
-  @Get()
+  @Get('consultar')
   findAll() {
     return this.produtosService.findAll();
   }
--- backendmf/src/produtos/produtos.service.ts
+++ backendmf/src/produtos/produtos.service.ts
@@ -1,15 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProdutoDto } from './dto/create-produto.dto';
 import { UpdateProdutoDto } from './dto/update-produto.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Produto } from './entities/produto.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ProdutosService {
+  constructor(
+    @InjectRepository(Produto)
+    private readonly produtoentity: Repository<Produto>,
+  ) {}
+
   create(createProdutoDto: CreateProdutoDto) {
     return 'This action adds a new produto';
   }
 
-  findAll() {
-    return `This action returns all produtos`;
+  async findAll() {
+    try {
+      const produtos = await this.produtoentity.find();
+      return produtos;
+    } catch (error) {
+      console.error(error);
+      throw new Error('Erro ao buscar produtos');
+    }
   }
 
   findOne(id: number) {
